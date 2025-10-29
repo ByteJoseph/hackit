c6e0a89b27f04c8e459a8c0153de3fd80f86fb81
Joseph Joseph <josephnjoseph43@gmail.com>
2025-10-29 18:49:06 +0530

    Add files via upload

--- polyaddlink17.c
+++ polyaddlink17.c
@@ -20,7 +20,7 @@ struct Node* insertNode(struct Node* head,int coef, int expo){
         return newNode;
     }
     struct Node* temp = head;
-    while(temp_next!=NULL){
+    while(temp->next!=NULL){
         temp = temp->next;
     }
     temp->next = newNode;
@@ -29,8 +29,8 @@ struct Node* insertNode(struct Node* head,int coef, int expo){
 struct Node* addPoly(struct Node* poly1, struct Node* poly2){
     struct Node* result = NULL;
     while(poly1 != NULL && poly2 != NULL){
-        if (poly1 ->expo > poly2->expo){
-            result = insertNode(result,poly->coeff,poly1->expo);
+        if (poly1->expo > poly2->expo){
+            result = insertNode(result,poly1->coeff,poly1->expo);
             poly1 = poly1->next;
         }
         else if(poly2->expo>poly1->expo){
@@ -38,7 +38,7 @@ struct Node* addPoly(struct Node* poly1, struct Node* poly2){
             poly2 = poly2->next;
         }
         else{
-            int sumcoef = poly1->coefff+poly2->coeff;
+            int sumcoef = poly1->coeff+poly2->coeff;
             if (sumcoef!=0){
                 result = insertNode(result,sumcoef,poly1->expo);
             }
@@ -70,4 +70,35 @@ void display(struct Node* poly){
         }
     }
     printf("\n");
+}
+struct Node* takeInput(){
+    int n, coeff, expo;
+    struct Node* poly = NULL;
+    printf("Enter the number of terms in the polynomial: ");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        printf("Enter coefficient and exponent of term %d: ",i+1);
+        scanf("%d %d",&coeff,&expo);
+        poly = insertNode(poly,coeff,expo);
+    }
+    return poly;
+}
+void main()
+{
+    struct Node* poly1 = NULL;
+    struct Node* poly2 = NULL;
+    struct Node* sum = NULL;
+
+    printf("Enter first polynomial:\n");
+    poly1 = takeInput();
+    printf("Enter second polynomial:\n");
+    poly2 = takeInput();
+    printf("First Polynomial: ");
+    display(poly1);
+    printf("Second Polynomial: ");
+    display(poly2);
+    sum = addPoly(poly1,poly2);
+    printf("Sum of Polynomials: ");
+    display(sum);
+
 }
